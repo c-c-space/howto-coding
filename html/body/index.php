895cca8a9a341792f844c5a800e973422d801010
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-04 01:02:58 +0900

    Update index.php

--- html/body/index.php
+++ html/body/index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no" />
   <script src="../../readme/index.js"></script>
-  <script src="../../js/window/online/script.js" defer></script>
+  <script src="../../js/window/online/script.js"></script>
   <base target="_blank" rel="noopener noreferrer">
   <link rel="stylesheet" href="../elements.css" />
   <link rel="stylesheet" href="../../readme/style.css" />
