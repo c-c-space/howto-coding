0ea43750f871fcd31e1b73fdc763dc80884ec4c4
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-04 22:54:11 +0900

    Update hello.php

--- php/include/hello.php
+++ php/include/hello.php
@@ -1,4 +1,5 @@
 <section id="hello" class="data_show">
   <p class="php">? php include('hello.php');</p>
   <p class="php">? php require('howto.html');</p>
+  <p class="php">? php readfile('howto.html');</p>
 </section>
