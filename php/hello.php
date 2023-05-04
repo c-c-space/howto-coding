253abbffc9d3f325f4ee5c0fa10295d5e346f6fb
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-04 23:39:05 +0900

    Update hello.php

--- php/hello.php
+++ php/hello.php
@@ -1,8 +1,7 @@
 <section id="hello" class="data_show php">
   ? php
-  <p>/* 文字列 を 変数へ格納 */</p>
+  <p><i>/* 文字列 を 変数へ格納 */</i></p>
   <p>$str_hello =  "Hello, World!";</p>
-  <br>
-  <p>// 文字列を出力</p>
+  <p><i>// 文字列を出力</i></p>
   <p>echo  $str_hello;</p>
 </section>
