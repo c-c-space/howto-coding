e0337b74bd68504456e11aa10bd5ea18cfb0dd6c
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-04 23:38:04 +0900

    php

---
+++ php/hello.php
@@ -0,0 +1,8 @@
+<section id="hello" class="data_show php">
+  ? php
+  <p>/* 文字列 を 変数へ格納 */</p>
+  <p>$str_hello =  "Hello, World!";</p>
+  <br>
+  <p>// 文字列を出力</p>
+  <p>echo  $str_hello;</p>
+</section>
---
+++ php/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no" />
+  <script src="../readme/index.js" defer></script>
+  <link rel="stylesheet" href="../cover/style.css" />
+  <link rel="stylesheet" href="../readme/style.css" />
+  <link rel="stylesheet" href="../hello/data_show.css" />
+  <link rel="stylesheet" href="../cover/mobile.css" media="screen and (max-width: 820px)">
+  <style>
+  .php::before {
+    content: '<';
+  }
+
+  .php::after {
+    content: ' ?>';
+  }
+
+  #links::before {
+    content:'参考資料';
+    font-size: 75%;
+    margin-bottom: 0.75rem;
+  }
+
+  #links::before {
+    font-family: "ipag", monospace;
+    display: block;
+    text-decoration: underline;
+  }
+  </style>
+</head>
+<body id="cover">
+  <main id="left">
+    <nav id="contents"></nav>
+    <?php include('hello.php'); ?>
+  </main>
+  <article id="right">
+    <?php require('howto.html'); ?>
+    <hr/>
+    <section id="links"></section>
+  </article>
+  <h1 id="title">PHP | Personal Home Page Tools</h1>
+
+  <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+  <script src="../hello/data_show.js"></script>
+</body>
+</html>
