e1f70f34adf31436f29d5b9dec86d343b365407d
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-05 18:59:30 +0900

    Update index.php

--- js/window/screen/index.php
+++ js/window/screen/index.php
@@ -48,7 +48,7 @@
     <aside id="contents"></aside>
     <article id="howto" class="readme">
       <?php require('howto.html'); ?>
-      <hr/>
+      <br/>
       <p id="lastModified"></p>
     </article>
     <hr/>
