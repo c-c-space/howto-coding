52a7fd3a5242abc0e842ad0a65492a43d4973b70
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-05 12:58:02 +0900

    js

--- js/window/online/network.js
+++ js/window/online/network.js
@@ -1,19 +1,19 @@
 "use strict"
 
-const connectionInfo = navigator.connection;
-if (connectionInfo !== undefined) {
+const connection = navigator.connection;
+if (connection !== undefined) {
   const init = function() {
-    document.getElementById('type').textContent = connectionInfo.type;
-    document.getElementById('effectiveType').textContent = connectionInfo.effectiveType;
-    document.getElementById('downlink').textContent = connectionInfo.downlink + ' Mb/s';
-    document.getElementById('downlinkMax').textContent = connectionInfo.downlinkMax + ' Mb/s';
-    document.getElementById('rtt').textContent = connectionInfo.rtt + ' ms';
+    document.getElementById('type').textContent = connection.type;
+    document.getElementById('effectiveType').textContent = connection.effectiveType;
+    document.getElementById('downlink').textContent = connection.downlink + ' Mb/s';
+    document.getElementById('downlinkMax').textContent = connection.downlinkMax + ' Mb/s';
+    document.getElementById('rtt').textContent = connection.rtt + ' ms';
   };
   init();
 
-  if ('onchange' in connectionInfo) {
-    connectionInfo.addEventListener('change', init);
-  } else if ('ontypechange' in connectionInfo) {
-    connectionInfo.addEventListener('typechange', init);
+  if ('onchange' in connection) {
+    connection.addEventListener('change', init);
+  } else if ('ontypechange' in connection) {
+    connection.addEventListener('typechange', init);
   }
 }
