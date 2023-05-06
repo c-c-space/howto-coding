c6863c16bc814f9801c48f86ecddf55de6fdc4d0
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-06 20:00:38 +0900

    Update howto.php

--- php/server/howto.php
+++ php/server/howto.php
@@ -1,24 +1,23 @@
 <section class="readme">
   <h2>
-    <i>通信に必要な情報</i>
+    <code>通信に必要な情報</code>
     <br/>
-    <u data-id="ip">IP</u>
-    <span id="ip" class="hide">
+    <u>IP</u>
+    <code>
       <?php
       echo $_SERVER['REMOTE_ADDR'];
       ?>
-    </span>
+    </code>
     <br/>
-    <u data-id="host">HOST</u>
-    <span id="host" class="hide">
+    <u>HOST</u>
+    <code>
       <?php
       $host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
       echo $host
       ?>
-      <br/>
-    </span>
-    <u data-id="port">PORT</u>
-    <span id="port" class="hide">
+    </code>
+    <u>PORT</u>
+    <span>
       <?php
       echo $_SERVER['REMOTE_PORT'];
       ?>
@@ -28,28 +27,23 @@
   <p>
     <i>これらの情報は、個人情報を直接含むような情報ではなく、安心・安全にインターネットを利用するための仕組みとして必ず発信しなければいけない情報です。</i>
   </p>
-  <hr/>
-  <h3>
-    <i>ブラウザ等情報</i>
-    <br/>
-    <span id="user" class="hide">
-      <br/>
-      <?php
-      echo $_SERVER['HTTP_USER_AGENT'];
-      ?>
-    </span>
-  </h3>
   <pre>
-    <font>$_SERVER['REMOTE_ADDR']</font>;
-    現在ページをみているユーザーの IP アドレス
+<font>$_SERVER['REMOTE_ADDR']</font>;
+現在ページをみているユーザーの IP アドレス
 
-    <font>gethostbyaddr($_SERVER['REMOTE_ADDR']</font>;
-    現在ページをみているユーザーの IP アドレスに対応するインターネットホスト名
+<font>gethostbyaddr($_SERVER['REMOTE_ADDR']</font>;
+現在ページをみているユーザーの IP アドレスに対応するインターネットホスト名
 
-    <font>$_SERVER['REMOTE_PORT']</font>;
-    ユーザーのマシンから Web サーバーへの通信に使用されているポート番号
+<font>$_SERVER['REMOTE_PORT']</font>;
+ユーザーのマシンから Web サーバーへの通信に使用されているポート番号
   </pre>
   <hr/>
+  <h3>
+    ブラウザ等情報<br/>
+    <?php
+    echo '<code>', $_SERVER['HTTP_USER_AGENT'], '</code>';
+    ?>
+  </h3>
   <p>
     <font>$_SERVER['HTTP_USER_AGENT']</font> は、
     ユーザーが使用しているブラウザの種類が保存される変数です。<br/>
@@ -63,9 +57,9 @@
   <hr/>
   <p id="lastModified"></p>
   <script>
-    let lastModified = document.querySelector('#lastModified');
+  let lastModified = document.querySelector('#lastModified');
 
-    lastModified.innerHTML =
-    'Last Modified: <time datetime="' + document.lastModified + '">' + document.lastModified + '</time>';
+  lastModified.innerHTML =
+  'Last Modified: <time datetime="' + document.lastModified + '">' + document.lastModified + '</time>';
   </script>
 </section>
