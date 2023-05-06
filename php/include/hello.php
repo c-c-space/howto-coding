2e63abd18946a225e2bc23c720e7a468fb65c18f
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-07 01:27:00 +0900

    php

--- php/include/hello.php
+++ php/include/hello.php
@@ -1,7 +1,7 @@
 <section id="hello">
-  <p class="php">? php include('hello.php');</p>
-  <p class="php">? php require('howto.html');</p>
+  <p class="php">?php include('hello.php');</p>
+  <p class="php">?php require('howto.html');</p>
   <br/>
   <p><i>指定したファイル・URLの全ての内容を標準出力へ書き出す</i></p>
-  <p class="php">? php readfile('howto.html');</p>
+  <p class="php">?php readfile('howto.html');</p>
 </section>
