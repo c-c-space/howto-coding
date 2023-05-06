e195dfa12c6b8a39e1c0d23e91c7e4dc75c49fb1
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-06 17:58:24 +0900

    php

--- php/hello.php
+++ php/hello.php
@@ -1,10 +1,10 @@
 <section id="hello">
-  <p class="php">? php<br/>
+  <p class="php">?php<br/>
     <i>/* 文字列 を 変数 へ格納 */</i><br/>
-    $hello =  "hello", "world!";<br/>
+    $name = 'World';<br/>
     <i>// 文字列を出力</i><br/>
     print 'Hello ';<br/>
-    echo "World", "!"<br/>
+    echo $name, "!"<br/>
   </p>
 
   <p><i># PHP からシステムに関する情報を取得する</i></p>
