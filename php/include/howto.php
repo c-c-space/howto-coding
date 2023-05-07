814707e759491c00ab81e06bbc0c928128efadf3
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-07 14:17:03 +0900

    Update howto.php

--- php/include/howto.php
+++ php/include/howto.php
@@ -4,19 +4,20 @@
     <font>require()</font>;
   </h2>
   <p>
-    ウェブサイトで同じパーツを使いまわす際などに、include() または require() を使って外部ファイルを読み込みます。<br/>
-    <i>
-      include と require の違いは、指定したファイルが見つからなかった際の処理です。<br/>
-      include は エラーが出ても その後の処理を続ける のに対して、 require は エラーが出た場合に PHP の 処理を中止します。
-    </i>
+    外部ファイルを読み込む PHP 関数 include と require の違いは、指定したファイルが見つからなかった際の処理です。<br/>
+    <i>include は エラーが出ても その後の処理を続ける のに対して、 require は エラーが出た場合に PHP の 処理を中止します。</i>
   </p>
-  <br>
+  <hr>
   <h3>
     <font>include_once()</font>;
     <font>require_once()</font>;
   </h3>
   <p>
-    ページ内に同じファイルを読み込まない場合は、include_once() または require_once() を使用します。
+    ページ内に同じファイルを読み込まない場合は、include_once() または require_once() を使用します。<br/>
+    <i>include_once と require_once の違いは include と require の違いと同じです。</i>
   </p>
-  <p>この方法は、WordPress の テーマ・テンプレートの作成などに使用されています。</p>
+  <p>
+    これらの関数は 複雑になったコードを分解して、外部ファイルとして読み込む際などに多く使用されます。<br/>
+    例えば、サイト内で共通して使用するヘッダーやフッターなどの部分を 別々のファイルとして管理し、簡易的なテンプレートを作成することができます。
+  </p>
 </section>
