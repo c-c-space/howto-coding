804a93f70eeb32019c8e245df27b7083b31e1489
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-05-07 14:57:56 +0900

    Update hello.php

--- php/include/hello.php
+++ php/include/hello.php
@@ -5,11 +5,6 @@
     <p>include('ファイル名');</p>
     <p><i>// include とほぼ同じですが、失敗した場合に E_COMPILE_ERROR レベルの致命的なエラーも発生する</i></p>
     <p>require('ファイル名');</p>
-    <p><i>// include または require で読み込まれたファイルの名前を配列として返す</i></p>
-    <p>$included_files = get_included_files()</p>
-    <p>
-      foreach ($included_files as $filename) {<br/>echo "$filename\n";<br/>}<br/>
-    </p>
   </div>
   <hr/>
   <div class="php">
@@ -18,11 +13,6 @@
     <p>include_once('ファイル名');</p>
     <p><i>require とほぼ同じですが、ファイルがすでに読み込まれているかどうかを PHP がチェックする</i></p>
     <p>require_once('ファイル名');</p>
-    <p><i>// get_included_files() のエイリアス</i></p>
-    <p>$required_files= get_required_files()</p>
-    <p>
-      foreach ($required_files as $requiredfilename) {<br/>echo "$requiredfilename\n";<br/>}<br/>
-    </p>
   </div>
 </section>
 <br>
